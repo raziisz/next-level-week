19004eb05e6791202431d5316653204c7d8f9f06
Luiz Felipe <raziel_libertino@hotmail.com>
2020-07-20 17:47:55 -0400

    validacao com celebrate

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import path from "path";
 import routes from './routes';
 
+import { errors } from 'celebrate';
 const app = express();
 
 app.use(cors());
@@ -11,5 +12,6 @@ app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
+app.use(errors());
 
 app.listen(3333);
