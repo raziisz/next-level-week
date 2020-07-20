61130dfee21927a82b8db44f96541c2ad140f581
Luiz Felipe <raziel_libertino@hotmail.com>
2020-07-20 16:42:30 -0400

    carregamento de files back and front

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -9,6 +9,8 @@ import api from '../../services/api';
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 
+import Dropzone from '../../components/DropZone';
+
 interface Item {
   id: number;
   title: string;
@@ -38,6 +40,7 @@ const CreatePoint = () => {
   const [city, setCity] = useState<string>('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
   
@@ -105,19 +108,25 @@ const CreatePoint = () => {
 
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
+
     const { name, email, whatsapp } = formData;
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    };
+
+    const data = new FormData();
+
+      data.append('name', name);
+      data.append('email', email);
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(','));
+      
+      if (selectedFile) {
+        data.append('image', selectedFile);
+      }
 
     try {
       await api.post('points', data);
@@ -144,6 +153,8 @@ const CreatePoint = () => {
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
